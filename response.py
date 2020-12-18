ea8d2a1efa8488e1d6117bb868865b015e8e48b6
Michael Katz <mikekatz04@gmail.com>
2020-12-18 22:44:59 +0100

    fixed sky unit vectors

--- response.py
+++ response.py
@@ -16,7 +16,6 @@ import h5py
 
 from scipy.interpolate import CubicSpline
 
-from few.summation.interpolatedmodesum import CubicSplineInterpolant
 from few.utils.constants import *
 import matplotlib.pyplot as plt
 
@@ -30,8 +29,6 @@ def get_factorial(n):
     return fact
 
 
-from few.waveform import FastSchwarzschildEccentricFlux
-
 from math import factorial
 
 factorials = np.array([factorial(i) for i in range(30)])
@@ -39,144 +36,12 @@ factorials = np.array([factorial(i) for i in range(30)])
 C_inv = 3.3356409519815204e-09
 
 
-class VariableFractionDelay:
-    def __init__(self, sampling_frequency, order=25):
-
-        self.sampling_frequency = sampling_frequency
-
-        self.order = order
-
-        if (self.order % 2) != 1:
-            raise ValueError("order must be odd.")
-
-        self.point_count = self.order + 1
-        self.half_point_count = int(self.point_count / 2)
-
-        self.h = self.half_point_count
-
-        self.min_delay = self.half_point_count / self.sampling_frequency
-
-        self.max_delay = 100000.0
-        self.max_integer_delay = int(np.floor(self.max_delay * self.sampling_frequency))
-
-        self.delays0 = []
-        self.delays1 = []
-
-    def update_coefficients(self, fraction):
-
-        e = fraction
-        h = self.half_point_count
-        A = 1.0
-
-        for i in range(1, h):
-            A *= (i + e) * (i + 1 - e)
-
-        denominator = factorials[h - 1] * factorials[h]
-        A /= denominator
-
-        self.A = A
-        self.B = 1.0 - e
-        self.C = e
-        self.D = e * (1.0 - e)
-
-        E = [0.0]
-        F = [0.0]
-        G = [0.0]
-
-        for j in range(1, h):
-            first_term = factorials[h - 1] / factorials[h - 1 - j]
-            second_term = factorials[h] / factorials[h + j]
-            value = first_term * second_term
-            if (j % 2) != 0:
-                value = -value
-
-            E.append(value)
-            F.append(j + e)
-            G.append(j + (1 - e))
-
-        self.E = np.asarray(E)
-        self.F = np.asarray(F)
-        self.G = np.asarray(G)
-
-    def var_frac_delay(self, integer_delay, fraction, input):
-        # st()
-        # clipped_delay = np.clip(
-        #    delay - input_start_time, self.min_delay, self.max_integer_delay
-        # )
-        # integer_delay = int(np.ceil(clipped_delay * sampling_frequency) - 1)
-        # fraction = 1.0 + integer_delay - clipped_delay * sampling_frequency
-        self.update_coefficients(fraction)
-
-        d = integer_delay
-        h = self.half_point_count
-        sumit = 0.0
-
-        for j in range(1, h):
-            sumit += self.E[j] * (
-                input[d + 1 + j] / self.F[j] + input[d - j] / self.G[j]
-            )
-
-        result = self.A * (self.B * input[d + 1] + self.C * input[d] + self.D * sumit)
-
-        return result
-
-    def __call__(self, input_in, i, dt, x0, x1, n, L, lam, beta, input_start_time):
-
-        t = i * dt
-        k = np.zeros(3, dtype=np.float)
-        u = np.zeros(3, dtype=np.float)
-        v = np.zeros(3, dtype=np.float)
-
-        cosbeta = np.cos(beta)
-        sinbeta = np.sin(beta)
-
-        coslam = np.cos(lam)
-        sinlam = np.sin(lam)
-
-        u[0] = -sinbeta * coslam
-        u[1] = -sinbeta * sinlam
-        u[2] = cosbeta
-        v[0] = sinlam
-        v[1] = -coslam
-        v[2] = 0.0
-        k[0] = -cosbeta * coslam
-        k[1] = -cosbeta * sinlam
-        k[2] = -sinbeta
-        u_dot_n = np.dot(u, n)
-        v_dot_n = np.dot(v, n)
-
-        xi_p = (u_dot_n * u_dot_n) - (v_dot_n * v_dot_n)
-        xi_c = 2.0 * u_dot_n * v_dot_n
-
-        k_dot_n = np.dot(k, n)
-        k_dot_x0 = np.dot(k, x0)
-        k_dot_x1 = np.dot(k, x1)
-
-        delay0 = t - L - k_dot_x0 * C_inv
-        delay1 = t - k_dot_x1 * C_inv
-
-        if link_i == 0:
-            self.delays0.append(t - delay0)
-            self.delays1.append(t - delay1)
-
-        clipped_delay0 = delay0 - input_start_time
-        integer_delay0 = int(np.ceil(clipped_delay0 * sampling_frequency)) - 1
-        fraction0 = 1.0 + integer_delay0 - clipped_delay0 * sampling_frequency
-
-        clipped_delay1 = delay1 - input_start_time
-        integer_delay1 = int(np.ceil(clipped_delay1 * sampling_frequency)) - 1
-        fraction1 = 1.0 + integer_delay1 - clipped_delay1 * sampling_frequency
-
-        temp = self.var_frac_delay(integer_delay0, fraction0, input_in)
-        hp_del0, hc_del0 = temp.real, temp.imag
-
-        temp = self.var_frac_delay(integer_delay1, fraction1, input_in)
-        hp_del1, hc_del1 = temp.real, temp.imag
-
-        pre_factor = 1.0 / (2 * (1.0 - k_dot_n))
-        large_factor = (hp_del0 - hp_del1) * xi_p + (hc_del0 - hc_del1) * xi_c
-
-        return pre_factor * large_factor
+def AET(X, Y, Z):
+    return (
+        (Z - X) / np.sqrt(2.0),
+        (X - 2.0 * Y + Z) / np.sqrt(6.0),
+        (X + Y + Z) / np.sqrt(3.0),
+    )
 
 
 class pyResponseTDI(object):
@@ -184,11 +49,10 @@ class pyResponseTDI(object):
         self,
         sampling_frequency,
         orbits_file="orbits.h5",
-        num_interp_points=100,
         order=25,
-        num_factorials=100,
         tdi="1st generation",
         max_t_orbits=3.15576e7,
+        tdi_chan="XYZ",
     ):
 
         self.sampling_frequency = sampling_frequency
@@ -199,8 +63,8 @@ class pyResponseTDI(object):
         self.half_order = int((order + 1) / 2)
 
         self.tdi = tdi
+        self.tdi_chan = tdi_chan
 
-        self.num_interp_points = num_interp_points
         self._fill_A_E()
         self._init_link_indices()
         self._init_orbit_information(orbits_file, max_t_orbits=max_t_orbits)
@@ -273,7 +137,7 @@ class pyResponseTDI(object):
             for key in f:
                 out[key] = f[key][:]
 
-        t_in = out['t']
+        t_in = out["t"]
         t_in = t_in - t_in[0]
         length_in = len(t_in)
 
@@ -314,7 +178,9 @@ class pyResponseTDI(object):
             L_in[i] = CubicSpline(t_in, L_in[i])(t_new)
 
         # get max buffer for projections
-        self.projection_buffer = int(np.max(x_in) * C_inv + np.max(np.abs(L_in))) + 4 * self.order
+        self.projection_buffer = (
+            int(np.max(x_in) * C_inv + np.max(np.abs(L_in))) + 4 * self.order
+        )
 
         self.x_in = xp.asarray(np.concatenate(x_in))
         self.n_in = xp.asarray(np.concatenate(n_in))
@@ -438,11 +304,12 @@ class pyResponseTDI(object):
 
                 i += 1
 
-
         self.max_delay = np.max(np.abs(self.t_data - delays[:]))
 
         # get necessary buffer for TDI
-        check_tdi_buffer = int(self.max_delay * self.sampling_frequency) + 4 * self.order
+        check_tdi_buffer = (
+            int(self.max_delay * self.sampling_frequency) + 4 * self.order
+        )
         self.tdi_buffer = 200
 
         assert check_tdi_buffer < self.tdi_buffer
@@ -470,7 +337,7 @@ class pyResponseTDI(object):
     def y_gw(self):
         return self.y_gw_flat.reshape(self.nlinks, -1)
 
-    def get_projections(self, input_in, beta, lam):
+    def get_projections(self, input_in, lam, beta):
 
         k = np.zeros(3, dtype=np.float)
         u = np.zeros(3, dtype=np.float)
@@ -488,12 +355,12 @@ class pyResponseTDI(object):
         coslam = np.cos(lam)
         sinlam = np.sin(lam)
 
-        u[0] = -sinbeta * coslam
-        u[1] = -sinbeta * sinlam
-        u[2] = cosbeta
-        v[0] = sinlam
-        v[1] = -coslam
-        v[2] = 0.0
+        v[0] = -sinbeta * coslam
+        v[1] = -sinbeta * sinlam
+        v[2] = cosbeta
+        u[0] = sinlam
+        u[1] = -coslam
+        u[2] = 0.0
         k[0] = -cosbeta * coslam
         k[1] = -cosbeta * sinlam
         k[2] = -sinbeta
@@ -506,29 +373,29 @@ class pyResponseTDI(object):
         input_in = xp.asarray(input_in)
 
         get_response_wrap(
-                y_gw,
-                k_in,
-                u_in,
-                v_in,
-                self.dt,
-                num_delays_proj,
-                self.link_space_craft_0_in,
-                self.link_space_craft_1_in,
-                input_in,
-                self.num_total_points,
-                self.order,
-                self.sampling_frequency,
-                self.buffer_integer,
-                self.A_in,
-                self.deps,
-                len(self.A_in),
-                self.E_in,
-                self.projection_buffer,
-                self.x_in,
-                self.n_in,
-                self.L_in,
-                self.num_orbit_inputs,
-            )
+            y_gw,
+            k_in,
+            u_in,
+            v_in,
+            self.dt,
+            num_delays_proj,
+            self.link_space_craft_0_in,
+            self.link_space_craft_1_in,
+            input_in,
+            self.num_total_points,
+            self.order,
+            self.sampling_frequency,
+            self.buffer_integer,
+            self.A_in,
+            self.deps,
+            len(self.A_in),
+            self.E_in,
+            self.projection_buffer,
+            self.x_in,
+            self.n_in,
+            self.L_in,
+            self.num_orbit_inputs,
+        )
 
         self.y_gw_flat = y_gw
         self.y_gw_length = num_delays_proj
@@ -548,142 +415,48 @@ class pyResponseTDI(object):
             for link_ind, sign in self.channels_no_delays[j]:
                 # TODO: check boundaries
                 self.delayed_links_flat[j] += (
-                    sign * self.y_gw[link_ind, self.tdi_buffer : self.num_delays_tdi + self.tdi_buffer]
+                    sign
+                    * self.y_gw[
+                        link_ind,
+                        self.tdi_buffer : self.num_delays_tdi + self.tdi_buffer,
+                    ]
                 )
 
         self.delayed_links_flat = self.delayed_links_flat.flatten()
 
         get_tdi_delays_wrap(
-                self.delayed_links_flat,
-                self.y_gw_flat,
-                self.y_gw_length,
-                self.num_orbit_inputs,
-                self.tdi_delays,
-                self.num_delays_tdi,
-                self.dt,
-                self.link_inds,
-                self.num_units,
-                self.num_channels,
-                self.order,
-                self.sampling_frequency,
-                self.buffer_integer,
-                self.A_in,
-                self.deps,
-                len(self.A_in),
-                self.E_in,
-                self.projection_buffer,
-                self.total_buffer,
-            )
+            self.delayed_links_flat,
+            self.y_gw_flat,
+            self.y_gw_length,
+            self.num_orbit_inputs,
+            self.tdi_delays,
+            self.num_delays_tdi,
+            self.dt,
+            self.link_inds,
+            self.num_units,
+            self.num_channels,
+            self.order,
+            self.sampling_frequency,
+            self.buffer_integer,
+            self.A_in,
+            self.deps,
+            len(self.A_in),
+            self.E_in,
+            self.projection_buffer,
+            self.total_buffer,
+        )
 
+        if self.tdi_chan == "XYZ":
+            return self.XYZ
 
-def galactic_binary_td_waveform(A, f, fdot, iota, phi0, psi, T=1.0, dt=10.0, buffer=0, return_t=False):
+        elif self.tdi_chan == "AET" or self.tdi_chan == "AE":
+            X, Y, Z = self.XYZ
+            A, E, T = AET(X, Y, Z)
+            if self.tdi_chan == "AET":
+                return A, E, T
 
-    cosps = xp.cos(2.*psi)
-    sinps = xp.sin(2.*psi)
-    cosiota = xp.cos(iota)
+            else:
+                return A, E
 
-    Aplus  = A*(1. + cosiota*cosiota);
-    Across = -2.0*A*cosiota;
-
-    plus_factor   =  Aplus*cosps - 1j * -Across*sinps
-    cross_factor   =  -Aplus*sinps - 1j * -Across*cosps
-
-    n = int(T * YRSID_SI / dt)
-    t_buffer = buffer * dt
-    t = xp.arange(0, n + 2 * buffer) * dt
-    t_in = t - t_buffer
-
-    # phi0 is phi(t = t_buffer)
-    inner = 2 * np.pi * (f * t_in + 1./2. * fdot * t_in **2) + phi0
-    plus = xp.cos(inner)
-    cross = xp.sin(inner)
-
-    h = plus_factor * plus + cross_factor * cross
-
-    if return_t:
-        return h, t
-
-    return h
-
-
-use_gpu = gpu
-
-num_pts_in = int(3e6)
-
-sampling_frequency = 0.1
-dt = 1 / sampling_frequency
-T = (num_pts_in * dt) / YRSID_SI
-
-
-order = 25
-response = pyResponseTDI(
-    sampling_frequency,
-    orbits_file="esa-orbits.h5",
-    order=order,
-    num_factorials=100,
-    tdi="1st generation",
-)
-
-
-A = 1e-19
-f = 1e-3
-fdot = 1e-16
-iota = np.pi/3.
-phi0 = np.pi/4.
-psi = np.pi/5.
-
-beta = 0.0
-lam = 0.0
-
-input_in = galactic_binary_td_waveform(A, f, fdot, iota, phi0, psi, T=T, dt=dt, buffer=response.total_buffer)
-
-#plt.plot(input_in.real)
-#plt.plot(input_in.imag)
-#plt.show()
-
-"""
-link_inds = np.concatenate([np.arange(6), np.arange(6)]).astype(dtype=np.int32)
-delay_factor = np.ones_like(link_inds)
-delay_factor[6:] = 2
-link_inds = delay_factor.copy()
-
-if gpu is False:
-    frac_delay_check = VariableFractionDelay(sampling_frequency, order=order)
-    out = []
-    for link_i in range(6):
-        out.append([])
-        for i in range(10000):
-            x0, x1, n, L = response.get_xnL(i, link_i)
-            tt1 = frac_delay_check(
-                input_in, i, dt, x0, x1, n, L, lam, beta, input_start_time
-            )
-            out[link_i].append(tt1)
-
-    breakpoint()
-    out = np.asarray(out)
-    import matplotlib.pyplot as plt
-
-
-    #check = np.load("test_resp.npy")
-
-    fig, ax = plt.subplots(3, 2)
-    fig.set_size_inches(10, 10)
-    ax = ax.ravel()
-    for link_i, ax_i in zip(range(6), ax):
-        ax_i.plot(out[link_i])
-        #ax_i.plot(check[link_i][:1000], ls="--")
-    plt.show()
-    breakpoint()
-"""
-if gpu:
-    num = 200
-    st = time.perf_counter()
-    for i in range(num):
-        response.get_projections(input_in, beta, lam)
-        response.get_tdi_delays()
-    et = time.perf_counter()
-
-    print('num delays:', len(input_in), (et - st)/num)
-    # print(i)
-
-breakpoint()
+        else:
+            raise ValueError("tdi_chan must be 'XYZ', 'AET' or 'AE'.")
